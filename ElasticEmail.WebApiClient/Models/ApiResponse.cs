8b9e5038d95a21cb0514a62eefea9ef5b4b726ba
Carlos Jimenez <carjimfa@gmail.com>
2018-11-19 20:40:45 +0100

    Moves apiResponse and voidApiResponse To models

---
+++ ElasticEmail.WebApiClient/Models/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticEmail.WebApiClient.Models
+{
+    public class ApiResponse<T>
+    {
+        public bool success = false;
+        public string error = null;
+        public T Data
+        {
+            get;
+            set;
+        }
+    }
+}
